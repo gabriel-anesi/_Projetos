0af8030a3e42bfa4cfd8caa285de0f1cae3ad78d
Gabriel Anesi de Oliveira <gabriel.anesi@gmail.com>
2022-10-13 15:54:28 -0300

    adicionada nova campeã Kasumi no sorteio

--- Sorteador de personagem/assets/js/script.js
+++ Sorteador de personagem/assets/js/script.js
@@ -1,10 +1,9 @@
-const paladinsChampions = ["Androxus", "Ash", "Atlas", "Azaan", "Barik", "Betty La Bomba", "Bomb King", "Buck", "Caspian", "Cassie", "Corvus", "Dredge", "Drogoz", "Evie", "Fernando", "Furia", "Grohk", "Grover", "Imani", "Inara", "Io", "Jenos", "Khan", "Kinessa", "Koga", "Lex", "Lian", "Lillith", "Maeve", "Makoa", "Mal'Damba", "Moji", "Octavia", "Pip", "Raum", "Rei", "Ruckus", "Saati", "Seris", "Sha Lin", "Skye", "Strix", "Talus", "Terminus", "Tiberius", "Torvald", "Tyra", "Vatu", "VII", "Viktor", "Vivian", "Vora", "Willo", "Yagorath", "Ying", "Zhin"];
+const paladinsChampions = ["Androxus", "Ash", "Atlas", "Azaan", "Barik", "Betty La Bomba", "Bomb King", "Buck", "Caspian", "Cassie", "Corvus", "Dredge", "Drogoz", "Evie", "Fernando", "Furia", "Grohk", "Grover", "Imani", "Inara", "Io", "Jenos", "Kasumi", "Khan", "Kinessa", "Koga", "Lex", "Lian", "Lillith", "Maeve", "Makoa", "Mal'Damba", "Moji", "Octavia", "Pip", "Raum", "Rei", "Ruckus", "Saati", "Seris", "Sha Lin", "Skye", "Strix", "Talus", "Terminus", "Tiberius", "Torvald", "Tyra", "Vatu", "VII", "Viktor", "Vivian", "Vora", "Willo", "Yagorath", "Ying", "Zhin"];
 
 function sortear(paladinsChampions) {
     for (let i = 0; i <= 56; i++){
     const randomChampion = paladinsChampions[Math.floor(Math.random() * paladinsChampions.length)]; 
     document.getElementById("campeao").innerText = randomChampion
-    return randomChampion
-
+    return randomChampion;
     }
 }
