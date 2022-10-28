8e9788b27a50e6424d4a5459239dc9f2e7c149cd
Gabriel Anesi de Oliveira <gabriel.anesi@gmail.com>
2022-10-28 18:01:37 -0300

    upando site

---
+++ People counter app/script.js
@@ -0,0 +1,23 @@
+let count = 0;
+let countEl = document.getElementById('count-el');
+let saveEl = document.getElementById('save-el');
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+    /* My version
+    document.getElementById('count-el').innerText = count = count + 1 */
+
+} 
+
+function decrement() {
+    count -= 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + ' - ';
+    saveEl.textContent += ' ' + countStr;
+    countEl.textContent = 0;
+    count = 0;
+}
