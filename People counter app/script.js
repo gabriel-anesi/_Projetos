1e3581b54e97c4214ba299c9aa9276b3d152f60e
Gabriel Anesi de Oliveira <gabriel.anesi@gmail.com>
2022-11-03 11:18:49 -0300

    changes to implement a sum feature

--- People counter app/script.js
+++ People counter app/script.js
@@ -1,6 +1,7 @@
 let count = 0;
 let countEl = document.getElementById('count-el');
 let saveEl = document.getElementById('save-el');
+let totalEl = document.getElementById('total-el');
 
 function increment() {
     count += 1
@@ -18,6 +19,7 @@ function decrement() {
 function save() {
     let countStr = count + ' - ';
     saveEl.textContent += ' ' + countStr;
+    totalEl.textContent = "TOTAL: "
     countEl.textContent = 0;
     count = 0;
 }
