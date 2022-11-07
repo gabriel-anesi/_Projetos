4c3b68ca5b47860cba877a4b1397f0b6ec9fce4f
Gabriel Anesi de Oliveira <gabriel.anesi@gmail.com>
2022-11-07 16:00:21 -0300

    js ok

--- People counter app/script.js
+++ People counter app/script.js
@@ -1,24 +1,24 @@
 let count = 0;
+let totalCount = 0;
 let countEl = document.getElementById('count-el');
 let saveEl = document.getElementById('save-el');
 let totalEl = document.getElementById('total-el');
 
 function increment() {
     count += 1
-    countEl.textContent = count
-    /* My version
-    document.getElementById('count-el').innerText = count = count + 1 */
+    countEl.textContent = count;
 } 
 
 function decrement() {
     count -= 1
-    countEl.textContent = count
+    countEl.textContent = count;
 }
 
 function save() {
     let countStr = count + ' - ';
     saveEl.textContent += ' ' + countStr;
-    totalEl.textContent = count
+    totalCount = totalCount + count;
+    totalEl.textContent = totalCount;
     countEl.textContent = 0;
     count = 0;
 }
